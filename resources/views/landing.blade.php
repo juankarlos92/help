c8e5dc557f836fdbf3807a3daf49881e9564ea18
Cuenca Pablo <148780611+cuencapm@users.noreply.github.com>
2023-12-29 13:47:41 -0500

    Actualizar página home, colores principales y eliminar elementos innecesarios

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta content="HelpDesk - Online Ticket Support" name="description" />
+    <meta content="Soporte HelpDesk - Online Ticket Support" name="description" />
     <meta name="website" content="https://w3bd.com" />
     <meta name="email" content="info@w3bd.com" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
